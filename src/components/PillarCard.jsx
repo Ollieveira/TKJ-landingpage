a13f9514ee089216248a90dda4cf57eb7f7bdf28
Ollieveira1 <willysaaoliveira@gmail.com>
2025-06-01 18:41:07 -0300

    Ajuste na borda do card PillarCard

--- src/components/PillarCard.jsx
+++ src/components/PillarCard.jsx
@@ -1,15 +1,15 @@
-import cardTexture from "../assets/card-texture.png"; 
+import cardTexture from "../assets/card-texture.png";
 
 export default function PillarCard({ title, text }) {
   return (
     // Container principal do card com posicionamento relativo
-    <div className="relative rounded-xl overflow-hidden shadow-lg">
+    <div className="relative rounded-2xl overflow-hidden shadow-lg">
       {/* 1. Cor de fundo sólida */}
       <div className="absolute inset-0 bg-red-main-dark "></div>
-      
+
       {/* 2. Imagem de textura por cima (deve ser uma imagem com transparência) */}
-      <div 
-        className="absolute inset-2 bg-cover bg-center rounded-2xl opacity-90" 
+      <div
+        className="absolute inset-2 bg-cover bg-center rounded-3xl opacity-90"
         style={{ backgroundImage: `url(${cardTexture})` }}
       ></div>
 
@@ -18,12 +18,8 @@ export default function PillarCard({ title, text }) {
         <h3 className="bg-red-main-dark text-center font-bold text-lg py-2 px-4 rounded-md shadow-md mb-4">
           {title}
         </h3>
-        <p className="text-text-main leading-relaxed">
-          {text}
-        </p>
+        <p className="text-text-main leading-relaxed">{text}</p>
       </div>
     </div>
   );
 }
-
-
